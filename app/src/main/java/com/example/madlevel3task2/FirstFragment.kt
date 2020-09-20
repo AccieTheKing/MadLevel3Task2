90e1fd3415d27bdf6d350baf2ec86d6406b31072
Acdaling <acdaling@gmail.com>
2020-09-20 18:50:47 +0200

    retrieve data from bundle with fragmentListener

--- app/src/main/java/com/example/madlevel3task2/FirstFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/FirstFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-    private val portals = listOf<Portal>()
+    private val portals = arrayListOf<Portal>()
     private val portalAdapter = PortalAdapter(portals)
 
     override fun onCreateView(
@@ -23,6 +27,25 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
 
+    private fun initViews() {
+        rvPortals.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        rvPortals.adapter = portalAdapter
+
+        observeAddPortalResult();
+    }
+
+    private fun observeAddPortalResult() {
+        setFragmentResultListener(REQ_PORTAL_KEY) { key, bundle ->
+            val portalTitle = bundle.getString(BUNDLE_PORTAL_TITLE).toString()
+            val portalUrl = bundle.getString(BUNDLE_PORTAL_URL).toString()
+
+            val portal = Portal(portalTitle, portalUrl)
+            portals.add(portal)
+
+            portalAdapter.notifyDataSetChanged()
+        }
     }
 }
